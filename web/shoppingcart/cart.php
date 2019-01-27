cdc41c5c3a0cd60413a43c043d8fd8cbdb6f1397
Keaton Sant <san16072@byui.edu>
2019-01-26 21:22:31 -0700

    created the pages needed to make my shopping app

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -1,26 +1,15 @@
-<?php
-session_start();
-$cart = $_SESSION["cart"] = array();
-var_dump($cart)
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Shop</title>
+    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
     <script>
     </script>
 </head>
 <body>
-    <div>
-    </div>
-    <div>
-    </div>
-    <div></div>
     
 </body>
 </html>
