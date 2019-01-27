47c3757495ccbfa0cf380e2a9450324a8285804f
Keaton Sant <san16072@byui.edu>
2019-01-26 23:48:58 -0700

    updated add php

--- web/shoppingcart/add.php
+++ web/shoppingcart/add.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-array_push($_SESSION['cart'], $_GET["product"]);
-var_dump($_SESSION);
+$_SESSION['cart'][] = $_GET["product"]
 var_dump($_SESSION['cart']);
 ?>
 <a href="browse.php">browse</a>
