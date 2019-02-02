65ca68e1970304fe43a2b41758579adc5f2f1f3c
Keaton Sant <san16072@byui.edu>
2019-02-01 18:38:54 -0700

    update css

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -52,8 +52,8 @@ session_start()
             echo "</div>";
         }        
     ?>
-    <div id="check_button">
-        <a href="checkout.php">Checkout</a>
+    <div>
+        <a id="check_button" href="checkout.php">Checkout</a>
     </div>
 </body>
 </html>
