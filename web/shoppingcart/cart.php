29a6cc5a4a10b540ef07f72e6c8c653c39b4b8d0
Keaton Sant <san16072@byui.edu>
2019-02-02 18:47:55 -0700

    thank you

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -7,7 +7,7 @@ session_start()
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Cart</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
     <script>
--- web/shoppingcart/confirm.php
+++ web/shoppingcart/confirm.php
@@ -8,10 +8,14 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Confirm</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
-    <script></script>
+    <script>
+        function redirect() {
+            window.location.href="browse.php";
+        };   
+    </script>
 </head>
 <body>
     <div>
@@ -20,19 +24,26 @@
         </header>
     </div>
     <div class="checkout_form">
-    <?php 
-        echo "<h2>" . $_POST["fname"] . " " . $_POST["lname"] . "</h2>";
-        echo "<h3>Address:</h3>";
-        echo $_POST["street"] . "<br>"; 
-        echo $_POST["city"] . ", " . $_POST["state"] . ", " . $_POST["zip"];
-        $total = 0;
-        foreach ($_SESSION['cart'] as $i => $value) {
-            echo $_SESSION['cart'][$i][0] . "        $" . $_SESSION['cart'][$i][1] . "<br>"; 
-            $total += $_SESSION['cart'][$i][1];       
-        };
-        echo "Total: $" . $total;
-
-    ?>
+        <form action="thankyou.php" method="post">
+            <?php 
+                echo "<h2>" . $_POST["fname"] . " " . $_POST["lname"] . "</h2>";
+                echo "<h3>Address:</h3>";
+                echo $_POST["street"] . "<br>"; 
+                echo $_POST["city"] . ", " . $_POST["state"] . ", " . $_POST["zip"];
+                $total = 0;
+                foreach ($_SESSION['cart'] as $i => $value) {
+                    echo $_SESSION['cart'][$i][0] . "        $" . $_SESSION['cart'][$i][1] . "<br>"; 
+                    $total += $_SESSION['cart'][$i][1];       
+                };
+                echo "Total: $" . $total;
+            ?>
+            <div >                
+                <input class="submit_checkout" type="submit" value="Order">                               
+            </div>                       
+        </form>
+        <div>
+            <input class="cancel_checkout" type="button" value="Cancel" onclick="redirect()">
+        </div>
     </div>
 </body>
 </html>
