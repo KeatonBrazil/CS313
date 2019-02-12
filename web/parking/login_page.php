4e63c9d5c7e7ff7b86f0ee4dda1296c83e2c9fd5
Keaton Sant <san16072@byui.edu>
2019-02-11 20:47:15 -0700

    updated login page

--- web/parking/login_page.php
+++ web/parking/login_page.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Parking | Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="parking.css">
     <script></script>
 </head>
 <body>
-    <div class="d-flex justify-content-center align-item-center container">
+    <div class="login_form">
         <form action="">
+            <span></span>
             <input type="text" placeholder="Enter Username">
             <input type="text" placeholder="Enter Password">
         </form>
