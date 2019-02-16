2fa098fb3a3823e1b29b035ee5f20e863ba72ba1
Keaton Sant <san16072@byui.edu>
2019-02-15 19:59:38 -0700

    update

--- web/parking/lot_detail.php
+++ web/parking/lot_detail.php
@@ -2,3 +2,30 @@
     require_once("parking_db.php");
     $db = get_db();
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Page Title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="parking.css">
+        <script></script>
+    </head>
+    <body>
+        <div>
+            <header class="parking_center">
+                <h1>BYU-I Parking</h1>
+            </header>       
+        </div>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li><a href="lot_view.php">Parking</a></li>
+                </ul>
+            </div>
+        </nav>        
+    </body>
+</html>
--- web/parking/lot_view.php
+++ web/parking/lot_view.php
@@ -29,23 +29,29 @@ $lots = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <ul class="nav navbar-nav">
-                <li><a href="enter_info.php">My Parking</a></li>
-                <li class="active"><a href="lot_view.php">Parking Status</a></li>
+                <li><a href="enter_info.php">Checkin</a></li>
+                <li class="active"><a href="lot_view.php">Parking</a></li>
                 </ul>
             </div>
         </nav>
         <div class="col-md-4 col-md-offset-4">
+            <table>
+                <tr>
+                    <th>Parking Lot</th>
+                    <th>Parking Pass</th>
+                </tr>
             <?php
                 foreach ($lots as $lot) {
                     $id = $lot['lot_id'];
                     $lot_loc = $lot['lot_location'];
                     $lot_pass = $lot['parking_pass'];
-
-                    echo "<li><a href='lot_detail.php?lot_id=$id'>$lot_loc - $lot_pass</a></li>";
+                    
+                    echo "<tr><td><a href='lot_detail.php?lot_id=$id'>$lot_loc</a></td><td>$lot_pass</td></tr>";
                 }
 
                 
             ?>
+            </table>
         </div>
     </body>
 </html>
