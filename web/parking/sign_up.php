0ae5c70e17023ff48a4e299dff85ee988411201d
Keaton Sant <san16072@byui.edu>
2019-02-23 09:21:20 -0700

    debugging

--- web/parking/sign_up.php
+++ web/parking/sign_up.php
@@ -44,6 +44,7 @@ $db = get_db();
                 <input type="text" id="email" name="e_mail"><br>
                 <label for="relation">School Relation</label><br>
                 <select name="" id="relation" name="member"><br>
+                    <option></option>
                     <option value="Student">Student</option>
                     <option value="Faculty">Faculty</option>
                     <option value="Visitor">Visitor</option>
