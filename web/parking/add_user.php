49f818a763917cd07600a5e07a919dd4c7d759a1
Keaton Sant <san16072@byui.edu>
2019-02-23 22:18:29 -0700

    signout

--- web/parking/add_user.php
+++ web/parking/add_user.php
@@ -8,9 +8,7 @@
     $major = htmlspecialchars($_POST['degree']);
     $preg = htmlspecialchars($_POST['preg']);
     $apt = htmlspecialchars($_POST['home']);
-    $match = $pass === $cpass;
-    echo $match;
-    var_dump($match);
+    
 
     if (!isset($user) || $user == "" || !isset($pass) || $pass == "" || !isset($email) || $email == "" || !isset($major) || $major == "")
     {
@@ -19,10 +17,8 @@
     }
     if ($pass === $comfpass) 
     {
-        /*
         header("Location: sign_up.php?noMatch=true");
-        die();
-        */
+        die();        
     }
 /*
     $hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
