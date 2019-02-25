69930e604078584fa251595dce6fb260431d3a5f
Keaton Sant <san16072@byui.edu>
2019-02-25 08:56:15 -0700

    checkin

--- web/parking/insert_checkin.php
+++ web/parking/insert_checkin.php
@@ -15,44 +15,28 @@ $db = get_db();
 
 $lot = htmlspecialchars($_POST['plot']);
 $pass = htmlspecialchars($_POST['ppass']);
-var_dump($lot);
-echo "<br>";
-echo "<br>";
-var_dump($pass);
-echo "<br>";
-echo "<br>";
+
 $query1 = 'SELECT lot_id FROM parking_lot WHERE lot_location=:lot AND parking_pass=:pass';
 $stmt = $db->prepare($query1);
 $stmt->bindValue(':lot', $lot, PDO::PARAM_STR);
 $stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
 $stmt->execute();
 $lot_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($lot_id);
-echo "<br>";
-echo "<br>";
+
 $query2 = 'SELECT member_id FROM member WHERE username=:username';
 $stmt = $db->prepare($query2);
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmt->execute();
 $member_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($member_id);
-echo "<br>";
-echo "<br>";
-var_dump($username);
-echo "<br>";
-echo "<br>";
 
-print_r($member_id);
-/*
 $query3 = 'INSERT INTO parking_info(start_at_date, start_at_time, lot_id, member_id) VALUES(CURRENT_DATE, CURRENT_TIME, :lot_id, :member_id)';
 $stmt = $db->prepare($query3);
-$stmt->bindValue(':lot_id', $lot_id, PDO::PARAM_INT);
-$stmt->bindValue(':member_id', $member_id, PDO::PARAM_INT);
+$stmt->bindValue(':lot_id', $lot_id[0]['lot_id'], PDO::PARAM_INT);
+$stmt->bindValue(':member_id', $member_id[0]['member_id'], PDO::PARAM_INT);
 $result = $stmt->execute();
-
 
 flush();
 header("Location:enter_info.php");
 die();
-*/
+
 ?>
