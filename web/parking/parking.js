55f3d909c517420e9bf47ccf541e76e4c8003c43
Keaton Sant <san16072@byui.edu>
2019-02-28 08:53:13 -0700

    enter info update

--- web/parking/parking.js
+++ web/parking/parking.js
@@ -1,14 +1,21 @@
 function change(x) {
     var doc = document.getElementById("pass");
+    var option1 = document.createElement("option");
+    var option2 = document.createElement("option");
+    var option3 = document.createElement("option");
+    option1.text = "North";
+    option2.text = "South";
+    option3.text = "Admin";
     if (x === "Taylor") {
-        doc.innerHTML = "<option value='North'>North</option>";
+        doc.add(option1);
     } else if (x === "STC") {
-        doc.innerHTML = "<option value='South'>South</option>";
+        doc.add(option2);
     } else if (x === "Snow") {
-        doc.innerHTML = "<option value='Admin'>North</option>"; 
+        doc.add(option3);
     } else if (x === "Kimball") {
-        doc.innerHTML = "<option value='North'>North</option><option value='Admin'>Admin</option>";
-    } //else if (x === "I-Center") {
-        //doc.innerHTML = "<option value='North'>North</option>";
-    //};
+        doc.add(option1);
+        doc.add(option3);
+    } else if (x === "I-Center") {
+        doc.add(option1)
+    };
 };
