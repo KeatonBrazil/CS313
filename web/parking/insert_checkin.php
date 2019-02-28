9797473b5aa176d6c7bed32c7d16f970c6460021
Keaton Sant <san16072@byui.edu>
2019-02-28 15:51:42 -0700

    debug

--- web/parking/insert_checkin.php
+++ web/parking/insert_checkin.php
@@ -20,7 +20,7 @@ $query = 'SELECT COUNT(info_id) AS cap FROM parking_info WHERE lot_id=:lot_id AN
 $stmt = $db->prepare($query);
 $stmt->bindValue(':lot_id', $lot_id, PDO::PARAM_INT);
 $stmt->execute();
-$lot = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$lots = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $query = 'SELECT capacity FROM parking_lot WHERE lot_id=:lot_id';
 $stmt = $db->prepare($query);
@@ -42,7 +42,7 @@ $stmt->bindValue(':username', $username, PDO::PARAM_STR);
 $stmt->execute();
 $member_id = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if ($lot[0]['cap'] <= $cap[0]['capacity']) {
+if ($lots[0]['cap'] <= $cap[0]['capacity']) {
 	$query3 = 'INSERT INTO parking_info(start_at_date, start_at_time, lot_id, member_id) VALUES(CURRENT_DATE, CURRENT_TIME, :lot_id, :member_id)';
 	$stmt = $db->prepare($query3);
 	$stmt->bindValue(':lot_id', $lot_id[0]['lot_id'], PDO::PARAM_INT);
