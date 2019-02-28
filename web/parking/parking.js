3ad211ad9c961b6a67fb58bb343473083dd4c810
Keaton Sant <san16072@byui.edu>
2019-02-28 09:11:31 -0700

    debug

--- web/parking/parking.js
+++ web/parking/parking.js
@@ -1,7 +1,27 @@
 function change(x) {
-    var doc = document.getElementById("pass");
-    
+    var docn = document.getElementById("north");
+    var docs = document.getElementById("south"); 
+    var doca = document.getElementById("admin");
+    console.log(x);
     if (x === "Taylor") {
-        doc.value = "<option>HI</option>";
-    }
+        docn.style.display = 'inline';
+        docs.style.display = 'none';
+        doca.style.display = 'none';
+    } else if (x === "STC") {
+        docn.style.display = 'none';
+        docs.style.display = 'inline';
+        doca.style.display = 'none';
+    } else if (x === "Snow") {
+        docn.style.display = 'none';
+        docs.style.display = 'none';
+        doca.style.display = 'inline';
+    } else if (x === "Kimball") {
+        docn.style.display = 'inline';
+        docs.style.display = 'none';
+        doca.style.display = 'inline';
+    } else if (x === "I-Center") {
+        docn.style.display = 'inline';
+        docs.style.display = 'none';
+        doca.style.display = 'none';
+    };
 };
