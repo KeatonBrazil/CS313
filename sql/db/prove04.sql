3bf92ab728bd85fa0ae8ad166968a9808c65bd58
Keaton Sant <san16072@byui.edu>
2019-02-28 18:41:19 -0700

    sending sql file to github

--- sql/db/prove04.sql
+++ sql/db/prove04.sql
@@ -5,18 +5,19 @@ DROP TABLE member;
 CREATE TABLE parking_lot (
     lot_id          SERIAL PRIMARY KEY,
     lot_location    VARCHAR (50) NOT NULL,
-    parking_pass    VARCHAR (25) NOT NULL
+    parking_pass    VARCHAR (25) NOT NULL,
+    capacity        INT NOT NULL
 );
 
 CREATE TABLE member (
     member_id       SERIAL PRIMARY KEY,
     username        VARCHAR (20) UNIQUE NOT NULL,
-    pass_word       VARCHAR (20) NOT NULL,
+    pass_word       VARCHAR (255) NOT NULL,
     email           VARCHAR (30) UNIQUE NOT NULL,
     school_relation VARCHAR (15) NOT NULL,
     major           VARCHAR (50) NOT NULL,
-    apt_name        VARCHAR (50) NOT NULL,
-    pregnant        BOOLEAN NOT NULL
+    apt_name        VARCHAR (50),
+    pregnant        BOOLEAN
 );
 
 CREATE TABLE parking_info (
@@ -29,11 +30,12 @@ CREATE TABLE parking_info (
     member_id       INT NOT NULL REFERENCES member(member_id)
 );
 
-INSERT INTO parking_lot 
-(lot_location, parking_pass)
-VALUES 
-('Taylor', 'North'), ('STC', 'South'), ('Snow', 'Admin'), ('Kymball', 'North'), ('Kymball', 'Admin'), ('I-Center', 'North');
 
+INSERT INTO parking_lot 
+(lot_location, parking_pass, capacity)
+VALUES 
+('Taylor', 'North', 50), ('STC', 'South', 200), ('Snow', 'Admin', 75), ('Kimball', 'North', 100), ('Kimball', 'Admin', 100), ('I-Center', 'North', 80);
+/*
 INSERT INTO member 
 (username, pass_word, email, school_relation, major, apt_name, pregnant)
 VALUES
@@ -49,3 +51,4 @@ VALUES
 ('2019-02-13', '09:30:00', '2019-02-13', '11:15:00', 5, 2),
 ('2019-02-13', '10:00:00', '2019-02-13', '14:30:00', 3, 1),
 ('2019-02-13', '08:00:00', '2019-02-13', '12:45:00', 2, 4);
+*/
